7d2ce66a63eafc33ce61754fad7f93b43b8c2e67
jlucator <115760568+jlucator@users.noreply.github.com>
2024-03-22 13:13:17 -0400

    Update myfirst.js

--- myfirst.js
+++ myfirst.js
@@ -1,6 +1,14 @@
-var http = require('http');
+const http = require('http');
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World!');
-}).listen(8080);
+const hostname = '192.168.0.32';
+const port = 8080;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.end('<h1>Hello World</h1>');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
